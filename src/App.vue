bf376949019b1dea10be3a42afbfb2464e4c6a28
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-10-15 15:03:14 -0600

    fix ico

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,60 @@
 <template>
   <TheHeader title="SS"></TheHeader>
-  <BaseButton>Click me</BaseButton>
-  <BaseButton class="flat">Click me</BaseButton>
-  <div class="faq-container">
-    <FaqCard class="faq">
-      <template v-slot:header> Headline </template>
-      This is a placeholder text for sampling the content of the label I am
-      making.
-    </FaqCard>
-    <FaqCard class="faq">
-      <template v-slot:header>
-        Can I customize there setups of the room for my event?
-      </template>
-      One approach to fix the delay while closing is to set the max-height
-      dynamically based on the actual content height. This way, the transition
-      will be smooth both when opening and closing the accordion.
-    </FaqCard>
-    <FaqCard class="faq">
-      <template v-slot:header>
-        Can I customize the setup of the conference room for my event?</template
-      >
-      One approach to fix the delay while closing is to set the max-height
-      dynamically based on the actual content height. This way, the transition
-      will be smooth both when opening and closing the accordion. One approach
-      to fix the delay while closing is to set the max-height dynamically based
-      on the actual content height. This way, the transition will be smooth both
-      when opening and closing the accordion. One approach to fix the delay
-      while closing is to set the max-height dynamically based on the actual
-      content height. This way, the transition will be smooth both when opening
-      and closing the accordion. One approach to fix the delay while closing is
-      to set the max-height dynamically based on the actual content height. This
-      way, the transition will be smooth both when opening and closing the
-      accordion. One approach to fix the delay while closing is to set the
-      max-height dynamically based on the actual content height. This way, the
-      transition will be smooth both when opening and closing the accordion. One
-      approach to fix the delay while closing is to set the max-height
-      dynamically based on the actual content height. This way, the transition
-      will be smooth both when opening and closing the accordion. One approach
-      to fix the delay while closing is to set the max-height dynamically based
-      on the actual content height. This way, the transition will be smooth both
-      when opening and closing the accordion. One approach to fix the delay
-      while closing is to set the max-height dynamically based on the actual
-      content height. This way, the transition will be THIS IS MAX CONTENT
-    </FaqCard>
+  <div
+    style="
+      display: grid;
+      justify-content: center; /* Horizontally center */
+      align-items: center;
+    "
+  >
+    <div>
+      <BaseButton>Click me</BaseButton>
+      <BaseButton class="flat">Click me</BaseButton>
+    </div>
+    <div class="faq-container">
+      <FaqCard class="faq">
+        <template v-slot:header> Headline </template>
+        This is a placeholder text for sampling the content of the label I am
+        making.
+      </FaqCard>
+      <FaqCard class="faq">
+        <template v-slot:header>
+          Can I customize there setups of the room for my event?
+        </template>
+        One approach to fix the delay while closing is to set the max-height
+        dynamically based on the actual content height. This way, the transition
+        will be smooth both when opening and closing the accordion.
+      </FaqCard>
+      <FaqCard class="faq">
+        <template v-slot:header>
+          Can I customize the setup of the conference room for my
+          event?</template
+        >
+        One approach to fix the delay while closing is to set the max-height
+        dynamically based on the actual content height. This way, the transition
+        will be smooth both when opening and closing the accordion. One approach
+        to fix the delay while closing is to set the max-height dynamically
+        based on the actual content height. This way, the transition will be
+        smooth both when opening and closing the accordion. One approach to fix
+        the delay while closing is to set the max-height dynamically based on
+        the actual content height. This way, the transition will be smooth both
+        when opening and closing the accordion. One approach to fix the delay
+        while closing is to set the max-height dynamically based on the actual
+        content height. This way, the transition will be smooth both when
+        opening and closing the accordion. One approach to fix the delay while
+        closing is to set the max-height dynamically based on the actual content
+        height. This way, the transition will be smooth both when opening and
+        closing the accordion. One approach to fix the delay while closing is to
+        set the max-height dynamically based on the actual content height. This
+        way, the transition will be smooth both when opening and closing the
+        accordion. One approach to fix the delay while closing is to set the
+        max-height dynamically based on the actual content height. This way, the
+        transition will be smooth both when opening and closing the accordion.
+        One approach to fix the delay while closing is to set the max-height
+        dynamically based on the actual content height. This way, the transition
+        will be THIS IS MAX CONTENT
+      </FaqCard>
+    </div>
   </div>
 </template>
 <style>
